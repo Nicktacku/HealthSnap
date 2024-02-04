36e0e15bc08e38ab51c89205813b7728c6499f65
Jinllamad <115025052+Jinllamad@users.noreply.github.com>
2024-02-04 23:11:31 +0800

    Created an updated contents in Results

--- health_snap/frontend/src/Result.js
+++ health_snap/frontend/src/Result.js
@@ -1,1 +1,33 @@
 import React, { useState } from "react";
+import "./Consultation.css"; //Import CSS file in Consultation
+
+function HealthResult () {
+    return (
+    <div>
+            <div className="container" id="page3">
+                <h1 style={{ marginLeft: "20px", marginTop: "20px" }}>HEALTH CONSULTATION RESULT</h1>
+                <ul className="list" style={{ marginLeft: "20px" }}>
+                    <h2 style={{ marginTop: "10px" }}>Based on your input, it is possible that you have:</h2>
+                    <li style={{ marginLeft: "30px" }} className="list-item">
+                        Common Cold - <span id="commonColdPercentage"></span>%
+                    </li>
+                    <li style={{ marginLeft: "30px" }} className="list-item">
+                        Influenza - <span id="influenzaPercentage"></span>%
+                    </li>
+                    <li style={{ marginLeft: "30px" }} className="list-item">
+                        Acute Bronchitis - <span id="acuteBronchitisPercentage"></span>%
+                    </li>
+                    <li style={{ marginLeft: "30px" }} className="list-item">
+                        Tonsillopharyngitis - <span id="tonsillopharyngitisPercentage"></span>%
+                    </li>
+                    <li style={{ marginLeft: "30px" }} className="list-item">
+                        Chickenpox - <span id="chickenpoxPercentage"></span>%
+                    </li>
+                </ul>
+            </div>
+    </div>
+    );
+};
+export default HealthResult;
+
+
--- health_snap/frontend/src/index.js
+++ health_snap/frontend/src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-import ConsultationP2 from "./ConsultationP2";
+import Result from "./Result";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ConsultationP2/>
+    <Result/>
   </React.StrictMode>
 );
 
